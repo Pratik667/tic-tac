8f36c790cac49329ce012a55f1eab8a70fd24236
Pratik667 <scientisrpsm2222@gmail.com>
2025-05-16 18:33:17 +0530

    Game over

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,28 +1,7 @@
-import React, { useState } from "react";
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-    // function handleSelectSquare (rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) =>
-    //         {const updateGameBoard = [...prevGameBoard.map(initialArray => [...initialArray])];
-    //     updateGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //     return updateGameBoard;});
-    //      onSelectSquare();
-    // }
-    for(const turn of turns){
-        const {square, player} = turn;
-        const {row, col} = square;
-        gameBoard[row][col] = player;
-    }
-
+export default function GameBoard({ onSelectSquare, board }) {
     return (<>
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {board.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
                         <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
